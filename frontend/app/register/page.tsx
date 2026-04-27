367fb4a86d707d9f93d9e4ecc4b3dcdca4f5aaa7
Tanay Sachdeva <tanaysachdeva873@gmail.com>
2026-04-27 22:46:39 +0530

    deleted the things that are wrong

--- frontend/app/register/page.tsx
+++
@@ -1,5 +0,0 @@
-import { AuthForm } from "@/components/dashboard/auth-form";
-
-export default function RegisterPage() {
-  return <AuthForm mode="register" />;
-}
--- frontend/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/tanay-io/RateSheild/frontend
-
-go 1.26.1
